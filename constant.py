6beb10d062649eacf1fb266dfc245c5d572dbef4
himanshu <himanshu.andel@yahoo.in>
2023-12-28 16:52:16 +0530

    learned till security

--- constant.py
+++ constant.py
@@ -4,3 +4,9 @@ class ModelName(str, Enum):
   alexnet = 'alexnet'
   resnet = 'resnet'
   lenet = 'lenet'
+
+
+class Tags(Enum):
+  """use it in method to create sapration of apis """
+  items = 'items'
+  users = 'users'
